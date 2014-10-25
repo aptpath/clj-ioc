30de2b6da594c35cfe734f287df5b0f49bdee1d4
gseaton <seatongs@gmail.com>
2014-10-25 11:59:21 -0700

    added protobuf dependencies for ci

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "http://github.com/aptpath/clj-ioc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[codox "0.8.10"]]
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :plugins [[codox "0.8.10"]
+            [lein-protobuf "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.flatland/protobuf "0.7.1"]])
